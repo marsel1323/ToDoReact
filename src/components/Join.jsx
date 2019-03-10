d9c95d48b8ee916a82c35d49c1af5b8f6e54471e
marsel1323 <amn132313@gmail.com>
2019-03-10 13:38:28 +0300

    add signup action, reducer and implement redux to the project

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,57 +1,66 @@
-import React, {Component} from 'react';
-import axios from 'axios';
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Join extends Component {
 	state = {
 		username: '',
 		email: '',
-		password: ''
+		password: '',
 	};
-	
-	handleSubmit = async (e) => {
-		e.preventDefault();
-		const response = await axios.post('http://localhost:8080/api/signup',{
-			username: this.state.username,
-			email: this.state.email,
-			password: this.state.password,
-		});
-		console.log(response);
-	};
-	
-	render() {
-		return (
-			<form className="ui form" onSubmit={this.handleSubmit}>
-				<div className="field">
-					<label>Username</label>
-					<input
-						type="text"
-						name="user-name"
-						placeholder="Username"
-						onChange={e => this.setState({username: e.target.value})}
-					/>
-				</div>
-				<div className="required field">
-					<label>Email</label>
-					<input
-						type="text"
-						name="email"
-						placeholder="Email address"
-						onChange={e => this.setState({email: e.target.value})}
-					/>
-				</div>
-				<div className="required field">
-					<label>Password</label>
-					<input
-						type="password"
-						name="password"
-						placeholder="Password"
-						onChange={e => this.setState({password: e.target.value})}
-					/>
-				</div>
-				<button className="ui green button">Sign up!</button>
-			</form>
-		);
-	}
+
+  onSubmit = formProps  => {
+    this.props.signup(formProps, () => {
+      this.props.history.push('/home');
+    });
+  };
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form className="ui form" onSubmit={handleSubmit(this.onSubmit)}>
+        <fieldset>
+          <label>Username</label>
+          <Field
+            name="username"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <fieldset>
+          <label>Email</label>
+          <Field
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <fieldset>
+          <label>Password</label>
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <div>{this.props.errorMessage}</div>
+        <button className="ui green button">Sign up!</button>
+      </form>
+    );
+  }
 }
 
-export default Join;
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.errorMessage };
+}
+
+
+export default compose(
+  connect(mapStateToProps, actions),
+  reduxForm({ form: 'signup' })
+)(Join);
